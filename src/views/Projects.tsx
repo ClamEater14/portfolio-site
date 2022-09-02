404879c8d9d34056485c7c75178fd4b8b7197038
Caleb Lam <caleb.lam@hotmail.com>
2022-09-02 02:26:13 -0400

    Add GitHub link (temporary)

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,10 +1,36 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function Projects() {
   return (
     <section id="projects">
       <Container fluid>
-        <h1 className="display-1 text-center title">Projects</h1>
+        <Row>
+          <Col>
+            <h1 className="display-1 text-center title">Projects</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="text-center m-0">Coming soon...</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="text-center m-0">
+              See my GitHub{' '}
+              <a
+                className="align-self-center"
+                href="https://github.com/clameater14"
+                rel="noopener noreferrer"
+                target="_blank"
+                color="#FFFFFF"
+              >
+                <u>here</u>
+              </a>
+              !
+            </p>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
