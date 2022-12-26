5aac4a8e6339455ef118f61d4f310816debfda9c
Caleb Lam <caleb.lam@hotmail.com>
2022-12-25 21:19:54 -0500

    Try adding CSP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import LoadingScreen from "../components/LoadingScreen";
+import { NextStrictCSP } from "next-strict-csp";
+
+const isProd = process.env.NODE_ENV === "production";
+const HeadCSP = isProd ? NextStrictCSP : Head;
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <HeadCSP>
+        {isProd && <meta httpEquiv="Content-Security-Policy" />}
+      </HeadCSP>
       <body>
         <div id="init-load">
           <LoadingScreen loading />
