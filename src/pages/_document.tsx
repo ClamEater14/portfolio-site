840b5b02dcf7309e440987dd20b4cae82584877e
Caleb Lam <caleb.lam@hotmail.com>
2022-12-25 21:23:50 -0500

    Revert "Try adding CSP"
    This reverts commit 5aac4a8e6339455ef118f61d4f310816debfda9c.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import LoadingScreen from "../components/LoadingScreen";
-import { NextStrictCSP } from "next-strict-csp";
-
-const isProd = process.env.NODE_ENV === "production";
-const HeadCSP = isProd ? NextStrictCSP : Head;
 
 export default function Document() {
   return (
     <Html lang="en">
-      <HeadCSP>
-        {isProd && <meta httpEquiv="Content-Security-Policy" />}
-      </HeadCSP>
+      <Head />
       <body>
         <div id="init-load">
           <LoadingScreen loading />
